72e7ff2c55fbba508bc984764b3ebc8f23277815
knight.gao <knightgao2@gmail.com>
2023-05-11 21:31:43 +0800

    init: 初始化

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      // preload: path.join(__dirname, 'preload.js')
+    },
+    icon: 'assets/default.png'  // 窗口小图标
+  })
+
+  mainWindow.loadURL('https://bill.devgaoy.cn/')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
