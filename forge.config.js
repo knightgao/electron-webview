caf250022c838454e742f9fe50264126d839bf7e
knight.gao <knightgao2@gmail.com>
2023-05-23 18:12:01 +0800

    feat: 支持m1打包

--- forge.config.js
+++ forge.config.js
@@ -1,22 +1,34 @@
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    osxsign: {}
+  },
   rebuildConfig: {},
+  packagerconfig: {    
+    icon: '/assets/default' 
+  },
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
       config: {},
     },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin','linux'],
-    },
+    // {
+    //   name: '@electron-forge/maker-zip',
+    //   platforms: ['darwin','linux'],
+    // },
     {
       name: '@electron-forge/maker-deb',
       config: {},
     },
     {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+      name: '@electron-forge/maker-dmg',
+      config: {
+        background: './assets/default.jpg',
+        format: 'ULFO'
+      }
+    }
+    // {
+    //   name: '@electron-forge/maker-zip',
+    //   config: {},
+    // },
   ],
 };
